e6d1a84b1696398118cad954c5710fe1b33a1d19
otman3339 <otman3339@gmail.com>
2016-05-17 18:09:24 +0800

    1 沉浸式状态栏设置

--- app/src/main/java/com/blue339/lolive/view/activity/MainActivity.java
+++ app/src/main/java/com/blue339/lolive/view/activity/MainActivity.java
@@ -1,10 +1,16 @@
 package com.blue339.lolive.view.activity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TabWidget;
+import android.widget.Toast;
 
 import com.blue339.lolive.R;
 
 public class MainActivity extends BaseActivity {
+
+    private long mExitTime = 0;
+    private TabWidget tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +21,22 @@ public class MainActivity extends BaseActivity {
 
     protected void initView() {
         initBaseView();
-//        toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if((System.currentTimeMillis() - mExitTime) > 2000) {
+            Toast.makeText(this, getString(R.string.tip_exit_app), Toast.LENGTH_SHORT).show();
+            mExitTime = System.currentTimeMillis();
+        } else {
+            finish();
+        }
     }
 }
--- app/src/main/java/com/blue339/lolive/view/activity/SplashActivity.java
+++ app/src/main/java/com/blue339/lolive/view/activity/SplashActivity.java
@@ -1,9 +1,31 @@
 package com.blue339.lolive.view.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.blue339.lolive.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by zeminwang on 16/5/16.
  */
 public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, 1500);
+    }
 }
